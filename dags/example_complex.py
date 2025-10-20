4af02b4227fbd12ccdf97defb573f0462a44a8fc
nealzsy <nealyouns@gmail.com>
2025-10-20 00:37:16 +0000

    add: bash_operator

---
+++ dags/example_complex.py
@@ -0,0 +1,23 @@
+from airflow.sdk import DAG
+import datetime
+import pendulum
+
+
+with models.DAG(
+    dag_id="example_complex",
+    schedule_interval=None,
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False,
+    tags=['example', 'example2', 'example3'],
+) as dag:
+    bash_t1 = BashOperator(
+        task_id="bash_t1",
+        bash_command="echo whoami",
+    )
+    bash_t2 = BashOperator(
+        task_id='bash_t2',
+        bash_command="echo $HOSTNAME",
+    )
+
+bash_t1 >> bash_t2
+
