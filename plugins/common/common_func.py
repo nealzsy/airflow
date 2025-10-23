679ad8a777afe82cd1052a152c4d69486399f4ee
nealzsy <nealyouns@gmail.com>
2025-10-23 04:58:16 +0000

    update: common_func.py

--- plugins/common/common_func.py
+++ plugins/common/common_func.py
@@ -5,3 +5,15 @@ def regist(name, sex, *args):
     print(f"name: {name})")
     print(f"sex: {sex}")
     print(f"args: {args}")
+
+def regist2(name, sex, *args, **kwargs):
+    print(f"name: {name})")
+    print(f"sex: {sex}")
+    print(f"args: {args}")
+    email = kwargs.get("email") or None
+    phone = kwargs.get("phone") or None
+    if email:
+        print(f"email: {email}")
+    if phone:
+        print(f"phone: {phone}")
+
